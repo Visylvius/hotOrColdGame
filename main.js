f833d1d2a834c9614588824529b97792f3e46871
Loren Barrick <lorenbarrick@gmail.com>
2015-10-01 18:58:06 -0700

    finish product

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
-var randomNumber = Math.floor(Math.random() * 100) + 1;
+function hotOrCold() {
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
 
-function guessRandomNumber(num) {
+function guessRandomNumber() {
   num = prompt('Guess a number between 1 and 100');
   num = Number(num);
-  var twentyToThirtyArr = [20,21,22,23,24,25,26,27,28,29,30];
-  var twentyToTenArr = [20,19,18,17,16,15,14,13,12,11,10];
   if(isNaN(num)) {
     prompt('That was not a number, please enter a number');
     return false;
   }
+
   if (num === randomNumber) {
     console.log('Great Job! That was the number!');
-  } else if (num - 50 || num + 50 < randomNumber) {
+  } else if (Math.abs(num - randomNumber) > 50) {
     console.log('Brrrr your freezing!');
-  } else if (num - 30 || num - 20 || num + 20 || num - 20 < randomNumber) {
+  } else if (Math.abs(num - randomNumber) > 30) {
     console.log('your getting warmer');
-  } else if (num) {
+  } else if (Math.abs(num - randomNumber) > 20) {
+    console.log('even warmer');
+  } else if (Math.abs(num - randomNumber) > 10) {
+    console.log('Your so close');
+  } else {
+    console.log('your burning hot');
+  }
 }
+
+  var randomNumber = getRandomNumber();
+  var num = -1;
+  console.log(randomNumber);
+  while (randomNumber != num) {
+    guessRandomNumber();
+  }
 }
